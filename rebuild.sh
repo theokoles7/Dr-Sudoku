89384dd29d0787b5f0b5dbb1b48e140ca7409c26
theokoles <gabrieltrahan777@hotmail.com>
2022-05-12 15:15:16 -0500

    Build Thu 12 May 2022 03:15:16 PM CDT

--- rebuild.sh
+++ rebuild.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
-echo "Pulling from master..."
-echo "PWD: $(pwd)"
-
-git pull origin master &
-wait
-
-build
-
-echo "Copying index.html and renaming to 404.html..."
-cd "../docs/" &
-wait
-echo "PWD: $(pwd)"
-
-cp index.html 404.html &
-wait
-
-echo "Pushing to repo..."
-cd ".." &
-wait
-echo "PWD: $(pwd)"
-
-git add . &
-wait
-
-git commit -m "Build $(date)" &
-wait
-
-git push origin prod &
-wait
-
-echo "Script complete!"
+function pull(){
+  echo "Pulling from master..."
+  cd "/home/theokoles/Projects/Custom Apps/Dr-Sudoku/"
+  echo "=> Navigated to $(pwd)"
+  git pull origin master &
+  wait
+  echo "=> Pull complete! -------------------------------"
+}
 
 function build(){
   echo "Executing build..."
-  cd ./ui/
-  echo "\tNavigated to ./ui"
+  cd "/home/theokoles/Projects/Custom Apps/Dr-Sudoku/ui/"
+  echo "=> Navigated to $(pwd)"
   ng build --output-path ../docs --base-href /Dr-Sudoku/ &
   wait
-  echo "\tBuild complete!"
+  echo "=> Build complete! ----------------------------"
 }
+
+function copyFile(){
+  echo "Copying index.html to 404.html..."
+  cd "/home/theokoles/Projects/Custom Apps/Dr-Sudoku/docs/"
+  echo "=> Navigated to $(pwd)"
+  cp index.html 404.html &
+  wait
+  echo "=> Copy complete! -----------------------------"
+}
+
+function commit(){
+  echo "Committing build to branch..."
+  cd "/home/theokoles/Projects/Custom Apps/Dr-Sudoku/"
+  echo "=> Navigated to $(pwd)"
+  git status & wait
+  git add . & wait
+  echo "=> Changes added to tree"
+  git status & wait
+  git commit -m "Build $(date)" & wait
+  echo "=> Changes committed to branch"
+  git status & wait
+  echo "=> Commit complete! ---------------------------"
+}
+
+function push(){
+  echo "Pushing to repository..."
+  cd "/home/theokoles/Projects/Custom Apps/Dr-Sudoku/"
+  echo "=> Navigated to $(pwd)"
+  git push origin master & wait
+  echo "=> Push complete! -----------------------------"
+}
+
+pull
+build
+copy
+commit
+push
+
+echo "******* Script complete! *******"
