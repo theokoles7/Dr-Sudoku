a7ca5080cf54cc56d90c4570c228d89b7c9e36a6
theokoles <gabrieltrahan777@hotmail.com>
2022-05-12 14:30:00 -0500

    Build Thu 12 May 2022 02:30:00 PM CDT

--- rebuild.sh
+++ rebuild.sh
@@ -2,20 +2,26 @@
 
 echo "Pulling from master..."
 cd /home/theokoles/Projects/Custom Apps/Dr-Sudoku
+
 git pull origin master
 
 echo "Building project..."
 cd /home/theokoles/Projects/Custom Apps/Dr-Sudoku/ui
+
 ng build --output-path ../docs --base-href /Dr-Sudoku/
 
 echo "Copying index.html and renaming to 404.html..."
 cd /home/theokoles/Projects/Custom Apps/Dr-Sudoku/docs
+
 cp index.html 404.html
 
 echo "Pushing to repo..."
 cd /home/theokoles/Projects/Custom Apps/Dr-Sudoku
+
 git add .
+
 git commit -m "Build $(date)"
+
 git push origin prod
 
 echo "Script complete!"
